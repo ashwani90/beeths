d4f6636ce4dd2a4ccf0d9496983d288274fb45b8
Ashwani <ashwani90125@gmail.com>
2025-06-30 00:17:50 +0530

    refactor code

---
+++ src/hooks/playNotes.js
@@ -0,0 +1,39 @@
+import { useState, useRef, useEffect } from "react";
+import * as Tone from 'tone';
+import { urlsObj } from '../data/notesUrl';
+
+const usePlayNotes = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const pianoSampler = useRef(null);
+
+  useEffect(() => {
+    pianoSampler.current = new Tone.Sampler({
+      urls: urlsObj,
+    }).toDestination();
+  }, []);
+
+  const playNotes = async (notes) => {
+    if (isPlaying) return; // Prevent multiple playbacks
+    setIsPlaying(true);
+    await Tone.start();
+
+    const now = Tone.now();
+    notes.forEach((note) => {
+      pianoSampler.current.triggerAttackRelease(
+        Tone.Frequency(note.midi, "midi"),
+        note.duration,
+        now + note.time
+      );
+    });
+
+    // Stop playback after the last note
+    const playbackDuration = Math.max(
+      ...notes.map((note) => note.time + note.duration)
+    );
+    setTimeout(() => setIsPlaying(false), playbackDuration * 1000);
+  };
+
+  return { isPlaying, playNotes };
+};
+
+export default usePlayNotes;
