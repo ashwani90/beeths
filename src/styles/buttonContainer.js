5e477579067bdd44f9a8b1096ad7d7620afb6a21
Ashwani <ashwani90125@gmail.com>
2025-07-02 01:48:31 +0530

    Add container

---
+++ src/styles/buttonContainer.js
@@ -0,0 +1,12 @@
+export const buttonContainerStyles = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    padding: "10px",
+    gap: "6px", // Spacing between buttons
+    background: "#fff",
+    borderRadius: "12px",
+    width: "fit-content",
+    margin: "auto",
+  };
