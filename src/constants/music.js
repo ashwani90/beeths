04a5d53e109805abbf1b4a5699648cb0bd992801
Ashwani <ashwani90125@gmail.com>
2025-07-03 00:05:56 +0530

    Add constants values

--- src/constants/music.js
+++ src/constants/music.js
@@ -1,2 +1,11 @@
 export const PIXELS_PER_SECOND = 100;
 export const NOTE_HEIGHT = 8;
+export const NOTE_HEIGHT_EDITOR = 20;
+
+export const NOTES_IN_OCTAVE = 12;
+export const PIANO_KEYS = 88;
+export const KEY_WIDTH = 40;
+export const INSTRUMENTS = ['piano', 'drums', 'violin'];
+
+export const DRUM_STEPS = 16;
+export const DRUM_NOTES = [36, 38, 42, 46]; 
